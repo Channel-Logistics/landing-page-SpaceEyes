7dee069bfb1bad695872bab1116eb2d6a005c50c
DavidSE03 <david@space-eyes.com>
2025-10-06 16:16:24 -0500

    feature: adds router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import Header from "./components/layout/Header"
+// src/App.tsx
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
-    <div className="relative h-screen">
+    <div className="flex flex-col min-h-screen bg-background text-foreground">
       <Header />
+
+      <main className="flex-grow pt-24">
+        <Routes>
+          <Route path="*" element={<h1 className="text-center mt-20 text-lg">404 - Page Not Found</h1>} />
+        </Routes>
+      </main>
+
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
