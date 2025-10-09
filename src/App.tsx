1b39bb05539f1f9a63fc0c2ab3dc1afa41e5bccb
ValeriaCarmona <valeria@space-eyes.com>
2025-10-09 10:06:07 -0500

    refactor: new implementations changes in ContactUs

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./components/pages/sections/Home";
 import Investor from "./components/pages/sections/Investor";
+import ContactUs from "./components/pages/usefulLinks/ContactUs";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Route element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/investor" element={<Investor />} />
+        <Route path="/contact-us" element={<ContactUs />} />
       </Route>
     </Routes>
   );
