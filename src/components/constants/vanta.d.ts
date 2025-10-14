8a33e8db711b96099d74f77b3480be20c82834f7
Daniel Eduardo Ramos Martinez <devlolo@MacBook-Pro-de-Daniel.local>
2025-10-14 11:06:09 -0500

    update code

--- src/components/constants/vanta.d.ts
+++ src/components/constants/vanta.d.ts
@@ -1,9 +1,6 @@
-// vanta.d.ts
-
-// 1. Define la interfaz para el objeto de configuración del efecto FOG (Niebla)
 interface FogOptions {
     el: HTMLElement | null;
-    THREE: any; // Usamos 'any' aquí, ya que el módulo 'three' ya tiene sus propios tipos.
+    THREE: any;
     mouseControls?: boolean;
     touchControls?: boolean;
     gyroControls?: boolean;
@@ -16,17 +13,12 @@ interface FogOptions {
     blurFactor?: number;
     speed?: number;
     zoom?: number;
-    // Otros parámetros de Vanta si los usas
 }
-
-// 2. Define la interfaz del objeto que devuelve Vanta.js
 interface VantaEffect {
     destroy: () => void;
     setOptions: (options: Partial<FogOptions>) => void;
-    // Puedes añadir otros métodos como resize() si los necesitas
 }
 
-// 3. Declara el módulo 'vanta/dist/vanta.fog.min'
 declare module 'vanta/dist/vanta.fog.min' {
     const FOG: (options: FogOptions) => VantaEffect;
     export default FOG;
